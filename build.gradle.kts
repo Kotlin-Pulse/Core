2595fc3bce0f694eb32ee15398615a7a2b630df0
CheeseTastisch <lian.hoerschlaeger@gmail.com>
2024-07-04 12:10:27 +0200

    feat: Setup gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.9.21"
+    `java-library`
+}
+
+allprojects {
+    group = "me.pulse"
+    version = "0.0.0"
+
+    repositories {
+        mavenCentral()
+    }
+
+    tasks {
+        compileJava {
+            options.encoding = Charsets.UTF_8.name()
+            options.release.set(21)
+        }
+
+        javadoc {
+            options.encoding = Charsets.UTF_8.name()
+        }
+
+        processResources {
+            filteringCharset = Charsets.UTF_8.name()
+        }
+
+        withType<KotlinCompile> {
+            kotlinOptions {
+                jvmTarget = "21"
+            }
+        }
+    }
+
+    kotlin {
+        jvmToolchain(21)
+    }
+}
