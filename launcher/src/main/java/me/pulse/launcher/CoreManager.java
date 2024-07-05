c4f478fe0aea8d5f60219bc4ba29970a3749819e
CheeseTastisch <lian.hoerschlaeger@gmail.com>
2024-07-05 10:38:33 +0200

    feat: use class instance as main instead of single static method

--- launcher/src/main/java/me/pulse/launcher/CoreManager.java
+++ launcher/src/main/java/me/pulse/launcher/CoreManager.java
@@ -4,6 +4,7 @@ import me.pulse.launcher.util.FileManager;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.jar.JarFile;
 
 public class CoreManager {
@@ -18,23 +19,36 @@ public class CoreManager {
 
     public void start(ClassLoader coreClassLoader) {
         String mainClassName;
+        String mainMethodName;
+
         try (var file = new JarFile(fileManager.getCore())) {
             var manifest = file.getManifest();
+
             mainClassName = manifest.getMainAttributes().getValue("Main-Class");
+            mainMethodName = manifest.getMainAttributes().getValue("Main-Method");
         } catch (IOException e) {
             throw new IllegalStateException("Couldn't read core.jar's Main-Class", e);
         }
 
         Class<?> mainClass;
+        Object mainInstance;
+
         try {
             mainClass = coreClassLoader.loadClass(mainClassName);
-        } catch (ClassNotFoundException e) {
-            throw new IllegalStateException("Couldn't find main class", e);
+
+            var constructor = mainClass.getDeclaredConstructor(File.class, String[].class);
+            constructor.setAccessible(true);
+
+            mainInstance = constructor.newInstance(fileManager.getDependenciesCache(), args);
+        } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException |
+                 InvocationTargetException e) {
+            throw new IllegalStateException("Couldn't find or load main class", e);
         }
 
         try {
-            var mainMethod = mainClass.getMethod("main", File.class, String[].class);
-            mainMethod.invoke(null, fileManager.getCore(), args);
+            var mainMethod = mainClass.getMethod(mainMethodName);
+            mainMethod.setAccessible(true);
+            mainMethod.invoke(mainInstance);
         } catch (Exception e) {
             throw new IllegalStateException("Couldn't start main class", e);
         }
